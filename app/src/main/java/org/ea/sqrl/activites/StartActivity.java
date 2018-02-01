f42cca985242bd3276aab8e1eac8fe46b3185fa6
Daniel Persson <mailto.woden@gmail.com>
2018-02-01 15:31:12 +0100

    Fixes for the start page.

--- app/src/main/java/org/ea/sqrl/activites/StartActivity.java
+++ app/src/main/java/org/ea/sqrl/activites/StartActivity.java
@@ -3,8 +3,10 @@ package org.ea.sqrl.activites;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.ea.sqrl.R;
 
@@ -14,6 +16,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        final TextView txtWelcomeMessage = findViewById(R.id.txtWelcomeMessage);
+        txtWelcomeMessage.setMovementMethod(new ScrollingMovementMethod());
 
         final Button btnScanSecret = findViewById(R.id.btnScanSecret);
         btnScanSecret.setOnClickListener(new View.OnClickListener() {
