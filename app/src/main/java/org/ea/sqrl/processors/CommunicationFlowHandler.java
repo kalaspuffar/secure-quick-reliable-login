2b92c7320e112148520f9488573ef3d3c0aae64c
Daniel Persson <mailto.woden@gmail.com>
2019-04-01 22:22:46 +0200

    Clear response before asking.

--- app/src/main/java/org/ea/sqrl/processors/CommunicationFlowHandler.java
+++ app/src/main/java/org/ea/sqrl/processors/CommunicationFlowHandler.java
@@ -180,6 +180,8 @@ public class CommunicationFlowHandler {
     }
 
     private void runAction(Action a) throws Exception {
+        commHandler.clearLastResponse();
+
         switch (a) {
             case LOGIN:
             case LOGIN_CPS:
