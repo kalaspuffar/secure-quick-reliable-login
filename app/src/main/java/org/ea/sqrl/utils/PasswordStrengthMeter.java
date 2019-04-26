c4f1d29d05b80278158527865e77dde1af6348c9
Alexander Hauser <alex.hauser@outlook.com>
2019-04-26 15:57:49 +0200

    Implemented changes suggested by Daniel while discussing the PR. Slight modification of the password strength calculation.

--- app/src/main/java/org/ea/sqrl/utils/PasswordStrengthMeter.java
+++ app/src/main/java/org/ea/sqrl/utils/PasswordStrengthMeter.java
@@ -16,16 +16,6 @@ import android.widget.Toast;
 
 import org.ea.sqrl.R;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
 /**
  * Calculates and displays a password strength rating.
  * Expects the password_strength_meter.xml to be present in the calling layout
@@ -35,8 +25,8 @@ import java.util.zip.ZipInputStream;
 public class PasswordStrengthMeter {
 
     private final int PW_MIN_LENGTH = 8;
-    private final int STRENGTH_POINTS_MIN_MEDIUM = 5;
-    private final int STRENGTH_POINTS_MIN_GOOD = 9;
+    private final int STRENGTH_POINTS_MIN_MEDIUM = 11;
+    private final int STRENGTH_POINTS_MIN_GOOD = 17;
     private final Context mContext;
     private ViewGroup mPassStrengthLayout;
     private CalcPasswordStrengthAsyncTask mLastCalcTask;
@@ -90,38 +80,47 @@ public class PasswordStrengthMeter {
             }
 
             PasswordStrengthResult result = new PasswordStrengthResult();
-            CharacterClass characterClass;
             String password = args[0];
 
-            result.PasswordLength = password.length();
-            if (result.PasswordLength <= 5) result.StrengthPoints = 0;
-            else if (result.PasswordLength <= 8) result.StrengthPoints = 1;
-            else if (result.PasswordLength <= 10) result.StrengthPoints = 2;
-            else if (result.PasswordLength <= 13) result.StrengthPoints = 5;
-            else if (result.PasswordLength <= 16) result.StrengthPoints = 10;
-            else result.StrengthPoints = 30;
+            result.passwordLength = password.length();
+            if (result.passwordLength > PW_MIN_LENGTH) result.strengthPoints = result.passwordLength;
+            else result.strengthPoints = (int)(result.passwordLength/2);
 
-            for (int i=0; i < result.PasswordLength; i++) {
+            for (int i = 0; i < password.length(); i++) {
 
                 char c = password.charAt(i);
 
-                if (c >= 65 && c <= 90) characterClass = CharacterClass.Uppercase;
-                else if (c >= 97 && c <= 122) characterClass = CharacterClass.Lowercase;
-                else if (c >= 48 && c <= 57) characterClass = CharacterClass.Digit;
-                else characterClass = CharacterClass.Symbol;
+                if (c >= 65 && c <= 90) {       // Uppercase
+                    if (!result.uppercaseUsed) {
+                        result.strengthPoints += 2;
+                        result.uppercaseUsed = true;
+                    }
+                }
+                else if (c >= 97 && c <= 122) { // Lowercase
+                    if (!result.lowercaseUsed) {
+                        result.lowercaseUsed = true;
+                    }
+                }
+                else if (c >= 48 && c <= 57) {  // Digit
+                    if (!result.digitsUsed) {
+                        result.strengthPoints += 2;
+                        result.digitsUsed = true;
+                    }
+                }
+                else {                          // Symbol
+                    if (!result.symbolsUsed) {
+                        result.strengthPoints += 2;
+                        result.symbolsUsed = true;
+                    }
+                }
 
-                Integer count = result.CharClassCounts.get(characterClass);
-                if (count == null) count = 0;
-
-                if (count == 0) result.StrengthPoints+=1; // increase only once per class
-                result.CharClassCounts.put(characterClass, count + 1);
-
+                if (result.allCharClassesUsed()) break; // No need to look any further
                 if (isCancelled()) return null;
             }
 
-            if (result.StrengthPoints < STRENGTH_POINTS_MIN_MEDIUM) result.Rating = PasswordRating.Poor;
-            else if (result.StrengthPoints < STRENGTH_POINTS_MIN_GOOD) result.Rating = PasswordRating.Medium;
-            else result.Rating = PasswordRating.Good;
+            if (result.strengthPoints < STRENGTH_POINTS_MIN_MEDIUM) result.rating = PasswordRating.POOR;
+            else if (result.strengthPoints < STRENGTH_POINTS_MIN_GOOD) result.rating = PasswordRating.MEDIUM;
+            else result.rating = PasswordRating.GOOD;
 
             // Last chance to detect cancellation
             if (isCancelled()) return null;
@@ -135,6 +134,8 @@ public class PasswordStrengthMeter {
             if (result == null) return;
 
             try {
+                Drawable ledGreen = ContextCompat.getDrawable(mContext, R.drawable.led_green);
+                Drawable ledRed   = ContextCompat.getDrawable(mContext, R.drawable.led_red);
                 TextView txtPasswordStrength = mPassStrengthLayout.findViewById(R.id.txtPasswordStrength);
                 TextView txtPasswordWarning = mPassStrengthLayout.findViewById(R.id.txtPasswordWarning);
                 ImageView imgUppercase = mPassStrengthLayout.findViewById(R.id.imgPasswordContainsUppercase);
@@ -144,46 +145,37 @@ public class PasswordStrengthMeter {
                 ProgressBar progressPwStrength = mPassStrengthLayout.findViewById(R.id.progressPasswordStrength);
 
                 progressPwStrength.setMax(STRENGTH_POINTS_MIN_GOOD);
-                progressPwStrength.setProgress(result.StrengthPoints);
+                progressPwStrength.setProgress(result.strengthPoints);
 
-                switch (result.Rating) {
-
-                    case Poor:
-                        txtPasswordStrength.setText(mContext.getText(R.string.password_strength_poor));
-                        txtPasswordStrength.setBackgroundColor(
-                                ContextCompat.getColor(mContext, R.color.password_strength_poor));
-                        break;
-
-                    case Medium:
-                        txtPasswordStrength.setText(mContext.getText(R.string.password_strength_medium));
-                        txtPasswordStrength.setBackgroundColor(
-                                ContextCompat.getColor(mContext, R.color.password_strength_medium));
-                        break;
-
-                    case Good:
-                        txtPasswordStrength.setText(mContext.getText(R.string.password_strength_good));
-                        txtPasswordStrength.setBackgroundColor(
-                                ContextCompat.getColor(mContext, R.color.password_strength_good));
-                        break;
+                if (result.rating == PasswordRating.POOR) {
+                    txtPasswordStrength.setText(mContext.getText(R.string.password_strength_poor));
+                    txtPasswordStrength.setBackgroundColor(
+                            ContextCompat.getColor(mContext, R.color.password_strength_poor));
+                }
+                else if (result.rating == PasswordRating.MEDIUM) {
+                    txtPasswordStrength.setText(mContext.getText(R.string.password_strength_medium));
+                    txtPasswordStrength.setBackgroundColor(
+                            ContextCompat.getColor(mContext, R.color.password_strength_medium));
+                }
+                else if (result.rating == PasswordRating.GOOD) {
+                    txtPasswordStrength.setText(mContext.getText(R.string.password_strength_good));
+                    txtPasswordStrength.setBackgroundColor(
+                            ContextCompat.getColor(mContext, R.color.password_strength_good));
                 }
 
-                ImageView iv = null;
-                for (CharacterClass cc : result.CharClassCounts.keySet()) {
-                    if (cc == CharacterClass.Lowercase) iv = imgLowercase;
-                    if (cc == CharacterClass.Uppercase) iv = imgUppercase;
-                    if (cc == CharacterClass.Digit) iv = imgDigits;
-                    if (cc == CharacterClass.Symbol) iv = imgSymbols;
+                if (result.lowercaseUsed) imgLowercase.setImageDrawable(ledGreen);
+                else imgLowercase.setImageDrawable(ledRed);
 
-                    int count = result.CharClassCounts.get(cc);
+                if (result.uppercaseUsed) imgUppercase.setImageDrawable(ledGreen);
+                else imgUppercase.setImageDrawable(ledRed);
 
-                    Drawable drawable = count > 0 ?
-                            ContextCompat.getDrawable(mContext, R.drawable.led_green) :
-                            ContextCompat.getDrawable(mContext, R.drawable.led_red);
+                if (result.digitsUsed) imgDigits.setImageDrawable(ledGreen);
+                else imgDigits.setImageDrawable(ledRed);
 
-                    if (iv != null) iv.setImageDrawable(drawable);
-                }
+                if (result.symbolsUsed) imgSymbols.setImageDrawable(ledGreen);
+                else imgSymbols.setImageDrawable(ledRed);
 
-                if (result.PasswordLength > 0 && result.PasswordLength < PW_MIN_LENGTH) {
+                if (result.passwordLength > 0 && result.passwordLength < PW_MIN_LENGTH) {
                     txtPasswordWarning.setText(R.string.short_password_warning);
                     txtPasswordWarning.setVisibility(View.VISIBLE);
                 }
@@ -198,33 +190,25 @@ public class PasswordStrengthMeter {
 
     class PasswordStrengthResult {
 
-        public int StrengthPoints = 0;
-        public int PasswordLength = 0;
-        public Map<CharacterClass, Integer> CharClassCounts;
-        public PasswordRating Rating = PasswordRating.Poor;
+        public int strengthPoints = 0;
+        public int passwordLength = 0;
 
-        public PasswordStrengthResult() {
+        public boolean lowercaseUsed = false;
+        public boolean uppercaseUsed = false;
+        public boolean digitsUsed = false;
+        public boolean symbolsUsed = false;
 
-            CharClassCounts = new HashMap<>();
+        public PasswordRating rating = PasswordRating.POOR;
 
-            for (CharacterClass cc : CharacterClass.values()) {
-                CharClassCounts.put(cc, 0);
-            }
+        public boolean allCharClassesUsed() {
+            return (lowercaseUsed && uppercaseUsed && digitsUsed && symbolsUsed);
         }
-    }
-
-    protected enum CharacterClass {
-
-        Lowercase,
-        Uppercase,
-        Digit,
-        Symbol
     }
 
     protected enum PasswordRating {
 
-        Poor,
-        Medium,
-        Good
+        POOR,
+        MEDIUM,
+        GOOD
     }
 }
