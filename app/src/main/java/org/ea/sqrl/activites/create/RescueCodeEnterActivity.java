34cdaae1a8f089a23a1e06a0b285b7400574d3b1
Alexander Hauser <alex.hauser@outlook.com>
2019-05-01 09:54:43 +0200

    Moved rescue code input validation code into its own helper class.
    Signed-off-by: Alexander Hauser <alex.hauser@outlook.com>

--- app/src/main/java/org/ea/sqrl/activites/create/RescueCodeEnterActivity.java
+++ app/src/main/java/org/ea/sqrl/activites/create/RescueCodeEnterActivity.java
@@ -4,16 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 
 import org.ea.sqrl.R;
-import org.ea.sqrl.processors.SQRLStorage;
+import org.ea.sqrl.utils.RescueCodeInputHelper;
 import org.ea.sqrl.utils.Utils;
-
-import java.util.List;
 
 /**
  *
@@ -21,13 +17,6 @@ import java.util.List;
  */
 public class RescueCodeEnterActivity extends AppCompatActivity {
     private static final String TAG = "RescueCodeEnterActivity";
-    private EditText txtRecoverCode1;
-    private EditText txtRecoverCode2;
-    private EditText txtRecoverCode3;
-    private EditText txtRecoverCode4;
-    private EditText txtRecoverCode5;
-    private EditText txtRecoverCode6;
-    private Button btnRescueCodeEnterNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +26,16 @@ public class RescueCodeEnterActivity extends AppCompatActivity {
 
         Utils.setLanguage(this);
 
-        txtRecoverCode1 = findViewById(R.id.txtRecoverCode1);
-        txtRecoverCode2 = findViewById(R.id.txtRecoverCode2);
-        txtRecoverCode3 = findViewById(R.id.txtRecoverCode3);
-        txtRecoverCode4 = findViewById(R.id.txtRecoverCode4);
-        txtRecoverCode5 = findViewById(R.id.txtRecoverCode5);
-        txtRecoverCode6 = findViewById(R.id.txtRecoverCode6);
+        ViewGroup rootLayout = findViewById(R.id.rescueCodeEntryActivityView);
+        Button btnRescueCodeEnterNext = findViewById(R.id.btnRescueCodeEnterNext);
 
-        btnRescueCodeEnterNext = findViewById(R.id.btnRescueCodeEnterNext);
+        RescueCodeInputHelper rescueCodeInputHelper = new RescueCodeInputHelper(this);
+        rescueCodeInputHelper.setDisplayErrors(true);
+        rescueCodeInputHelper.setStatusChangedListener(successfullyCompleted -> {
+            btnRescueCodeEnterNext.setEnabled(successfullyCompleted);
+        });
+        rescueCodeInputHelper.register(rootLayout, btnRescueCodeEnterNext);
+
         btnRescueCodeEnterNext.setEnabled(false);
         btnRescueCodeEnterNext.setOnClickListener(v -> {
             this.finish();
@@ -53,52 +44,5 @@ public class RescueCodeEnterActivity extends AppCompatActivity {
 
         boolean runningTest = getIntent().getBooleanExtra("RUNNING_TEST", false);
         if(runningTest) return;
-
-        SQRLStorage storage = SQRLStorage.getInstance(RescueCodeEnterActivity.this.getApplicationContext());
-        List<String> rescueList = storage.getTempShowableRescueCode();
-
-        setListener(txtRecoverCode1, rescueList);
-        setListener(txtRecoverCode2, rescueList);
-        setListener(txtRecoverCode3, rescueList);
-        setListener(txtRecoverCode4, rescueList);
-        setListener(txtRecoverCode5, rescueList);
-        setListener(txtRecoverCode6, rescueList);
-
-    }
-
-    private void setListener(EditText code, List<String> rescueList) {
-        code.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() == 4) {
-                    boolean incorrect =
-                            checkEditText(txtRecoverCode1, rescueList.get(0)) ||
-                            checkEditText(txtRecoverCode2, rescueList.get(1)) ||
-                            checkEditText(txtRecoverCode3, rescueList.get(2)) ||
-                            checkEditText(txtRecoverCode4, rescueList.get(3)) ||
-                            checkEditText(txtRecoverCode5, rescueList.get(4)) ||
-                            checkEditText(txtRecoverCode6, rescueList.get(5));
-                    btnRescueCodeEnterNext.setEnabled(!incorrect);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-    }
-
-    private boolean checkEditText(EditText code, String verify) {
-        if (code.getText().toString().equals(verify)) {
-            code.setError(null);
-            return false;
-        } else {
-            if(code.getText().length() > 0) {
-                code.setError("Incorrect");
-            }
-            return true;
-        }
     }
 }
---
+++ app/src/main/java/org/ea/sqrl/utils/RescueCodeInputHelper.java
@@ -0,0 +1,149 @@
+package org.ea.sqrl.utils;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import org.ea.sqrl.R;
+import org.ea.sqrl.processors.SQRLStorage;
+
+import java.util.List;
+
+/**
+ * Helper class for handling rescue code input and validation.
+ * Expects the rescue_code_input.xml to be present in the calling layout
+ *
+ * @author Alexander Hauser (alexhauser)
+ */
+public class RescueCodeInputHelper {
+
+    public interface StatusChangedListener {
+        void onEvent(boolean successfullyCompleted);
+    }
+
+    private static final String TAG = "RescueCodeInputHelper";
+    private Context mContext;
+    private ViewGroup mLayoutRoot;
+    private EditText mTxtRecoverCode1;
+    private EditText mTxtRecoverCode2;
+    private EditText mTxtRecoverCode3;
+    private EditText mTxtRecoverCode4;
+    private EditText mTxtRecoverCode5;
+    private EditText mTxtRecoverCode6;
+    private SQRLStorage mSqrlStorage;
+    private boolean mDisplayErrors = false;
+    private StatusChangedListener mStatusChangedListener;
+
+
+    /**
+     * Creates a RescueCodeInputHelper object.
+     *
+     * @param context The context of the caller.
+     */
+    public RescueCodeInputHelper (Context context) {
+
+        mContext = context;
+        mSqrlStorage = SQRLStorage.getInstance(mContext);
+    }
+
+    /**
+     * Registers the RescueCodeInputHelper to set event handlers on the rescue code input form
+     * and enables the corresponding events.
+     *
+     * @param rootLayout The root layout containing the rescue_code_input layout as well as the nextFocusDown view.
+     * @param nextFocusDown The View that should receive focus after the last rescue code field has been filled out.
+     */
+    public void register(ViewGroup rootLayout, @Nullable View nextFocusDown) {
+
+        mLayoutRoot = rootLayout;
+
+        mTxtRecoverCode1 = mLayoutRoot.findViewById(R.id.txtRecoverCode1);
+        mTxtRecoverCode2 = mLayoutRoot.findViewById(R.id.txtRecoverCode2);
+        mTxtRecoverCode3 = mLayoutRoot.findViewById(R.id.txtRecoverCode3);
+        mTxtRecoverCode4 = mLayoutRoot.findViewById(R.id.txtRecoverCode4);
+        mTxtRecoverCode5 = mLayoutRoot.findViewById(R.id.txtRecoverCode5);
+        mTxtRecoverCode6 = mLayoutRoot.findViewById(R.id.txtRecoverCode6);
+
+        if (nextFocusDown != null) {
+            mTxtRecoverCode6.setNextFocusDownId(nextFocusDown.getId());
+        }
+
+        List<String> rescueList = mSqrlStorage.getTempShowableRescueCode();
+
+        setEditTextListener(mTxtRecoverCode1, rescueList);
+        setEditTextListener(mTxtRecoverCode2, rescueList);
+        setEditTextListener(mTxtRecoverCode3, rescueList);
+        setEditTextListener(mTxtRecoverCode4, rescueList);
+        setEditTextListener(mTxtRecoverCode5, rescueList);
+        setEditTextListener(mTxtRecoverCode6, rescueList);
+    }
+
+    /**
+     * Enables or disables showing input errors in the input form's UI.
+     *
+     * @param displayErrors Set to true if input errors should be displayed, otherwise to false.
+     */
+    public void setDisplayErrors(boolean displayErrors) {
+
+        mDisplayErrors = displayErrors;
+    }
+
+    /**
+     * Registers a callback to be invoked when the status of the rescue code input changes.
+     *
+     * @param listener The callback method that should be executed.
+     */
+    public void setStatusChangedListener(StatusChangedListener listener) {
+
+        mStatusChangedListener = listener;
+    }
+
+    private void setEditTextListener(EditText code, List<String> rescueList) {
+
+        code.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                boolean correct =
+                        checkEditText(mTxtRecoverCode1, rescueList.get(0)) &&
+                        checkEditText(mTxtRecoverCode2, rescueList.get(1)) &&
+                        checkEditText(mTxtRecoverCode3, rescueList.get(2)) &&
+                        checkEditText(mTxtRecoverCode4, rescueList.get(3)) &&
+                        checkEditText(mTxtRecoverCode5, rescueList.get(4)) &&
+                        checkEditText(mTxtRecoverCode6, rescueList.get(5));
+                if(mStatusChangedListener != null) {
+                    mStatusChangedListener.onEvent(correct);
+                }
+            }
+        });
+    }
+
+    private boolean checkEditText(EditText code, String verify) {
+
+        String check = code.getText().toString();
+        if(check.length() != 4) return false;
+
+        if (check.equals(verify)) {
+            code.setError(null);
+            View nextFocusDown = mLayoutRoot.findViewById(code.getNextFocusDownId());
+            if (nextFocusDown != null) nextFocusDown.requestFocus();
+            return true;
+        } else {
+            if(mDisplayErrors) {
+                code.setError(mContext.getResources().getString(
+                        R.string.rescue_code_incorrect));
+            }
+            return false;
+        }
+    }
+}
