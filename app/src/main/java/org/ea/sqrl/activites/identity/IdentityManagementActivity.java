181d80f66f5016f4873c78b114c201b4ff09cdea
Alex Hauser <alex@hausers.cc>
2019-05-31 12:59:22 +0200

    Add password options as popup menu.

--- app/src/main/java/org/ea/sqrl/activites/identity/IdentityManagementActivity.java
+++ app/src/main/java/org/ea/sqrl/activites/identity/IdentityManagementActivity.java
@@ -8,18 +8,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.PopupMenu;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import org.ea.sqrl.R;
 import org.ea.sqrl.activites.SettingsActivity;
@@ -28,22 +21,16 @@ import org.ea.sqrl.activites.base.LoginBaseActivity;
 import org.ea.sqrl.activites.create.CreateIdentityActivity;
 import org.ea.sqrl.activites.create.RekeyIdentityActivity;
 import org.ea.sqrl.processors.CommunicationFlowHandler;
-import org.ea.sqrl.processors.CommunicationHandler;
-import org.ea.sqrl.processors.SQRLStorage;
-import org.ea.sqrl.utils.Utils;
-
-import java.util.regex.Matcher;
 
 /**
- * This main activity is the hub of the application where the user lands for daily use. It should
- * make it easy to reach all the functions you use often and hide things that you don't need
- * regularly.
+ * This activity is the central hub for all identity management functionality.
  *
  * @author Daniel Persson
  */
 public class IdentityManagementActivity extends LoginBaseActivity {
     private static final String TAG = "IdentityManagementActivity";
 
+    private ImageButton btnIdentityOptions = null;
     private boolean importIdentity = false;
 
     @Override
@@ -81,24 +68,9 @@ public class IdentityManagementActivity extends LoginBaseActivity {
                 v -> startActivity(new Intent(this, ImportOptionsActivity.class))
         );
 
-        /*
-        final Button btnChangePassword = findViewById(R.id.btnChangePassword);
-        btnChangePassword.setOnClickListener(
-                v -> startActivity(new Intent(this, ChangePasswordActivity.class))
-        );
-
-        final Button btnReset = findViewById(R.id.btnReset);
-        btnReset.setOnClickListener(
-                v -> startActivity(new Intent(this, ResetPasswordActivity.class))
-        );
-
-        final Button btnRekey = findViewById(R.id.btnRekey);
-        btnRekey.setOnClickListener(v -> startActivity(new Intent(this, RekeyIdentityActivity.class)));
-        */
-
-        final ImageButton btnOptions = findViewById(R.id.btnOptions);
-        btnOptions.setOnClickListener(v -> {
-            PopupMenu popup = new PopupMenu(IdentityManagementActivity.this, btnOptions);
+        btnIdentityOptions = findViewById(R.id.btnIdentityOptions);
+        btnIdentityOptions.setOnClickListener(v -> {
+            PopupMenu popup = new PopupMenu(IdentityManagementActivity.this, btnIdentityOptions);
             popup.getMenuInflater()
                     .inflate(R.menu.menu_id_management_options, popup.getMenu());
 
@@ -108,23 +80,18 @@ public class IdentityManagementActivity extends LoginBaseActivity {
                     case R.id.action_idm_settings:
                         startActivity(new Intent(this, SettingsActivity.class));
                         break;
-
                     case R.id.action_idm_rename:
                         startActivity(new Intent(this, RenameActivity.class));
                         break;
-
                     case R.id.action_idm_remove:
                         removeIdentity();
                         break;
-
                     case R.id.action_idm_export:
                         startActivity(new Intent(this, ExportOptionsActivity.class));
                         break;
-
                     case R.id.action_idm_password_options:
-                        //startActivity(new Intent(this, PasswordOptionsActivity.class));
+                        showPasswordOptionsMenu();
                         break;
-
                     default:
                         break;
                 }
@@ -165,6 +132,32 @@ public class IdentityManagementActivity extends LoginBaseActivity {
                 updateSpinnerData(currentId);
             }
         }
+    }
+
+    private void showPasswordOptionsMenu() {
+        PopupMenu popup = new PopupMenu(IdentityManagementActivity.this, btnIdentityOptions);
+        popup.getMenuInflater()
+                .inflate(R.menu.menu_id_password_options, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+
+                case R.id.action_idm_pw_change_password:
+                    startActivity(new Intent(this, ChangePasswordActivity.class));
+                    break;
+                case R.id.action_idm_pw_reset_password:
+                    startActivity(new Intent(this, ResetPasswordActivity.class));
+                    break;
+                case R.id.action_idm_pw_rekey:
+                    startActivity(new Intent(this, RekeyIdentityActivity.class));
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        });
+
+        popup.show();
     }
 
     private void removeIdentity() {
