8ca3b7836cf3764d3c99f51fcdb623be9ee4b6a1
Alexander Hauser <alex@hausers.cc>
2019-12-31 10:34:09 +0100

    Add tests using the new base56 vectors to Base56UnitTest

--- app/src/test/java/org/ea/sqrl/Base56UnitTest.java
+++ app/src/test/java/org/ea/sqrl/Base56UnitTest.java
@@ -3,6 +3,8 @@ package org.ea.sqrl;
 import org.ea.sqrl.utils.EncryptionUtils;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -34,5 +36,64 @@ public class Base56UnitTest {
         String encoded = EncryptionUtils.encodeBase56(decodedBytes);
 
         assertEquals("Encoding should be the same before and after", testIdentity, encoded);
+    }
+
+    @Test
+    public void testEncodeAndDecodeFullFormatVectors() throws Exception {
+        List<List<String>> vectors = TestHelper.parseVectorCsvFile(
+                "base56-full-format-vectors.txt", true, true );
+
+        int vectorNumber = 1;
+        for (List<String> vector: vectors) {
+            byte[] input = EncryptionUtils.hex2Byte(vector.get(1));
+            String expectedResult = vector.get(2).replace(" ", "").replace("\\n", "");
+
+            String encoded = EncryptionUtils.encodeBase56(input);
+            byte[] decoded = EncryptionUtils.reverse(EncryptionUtils.decodeBase56(encoded));
+
+            assertEquals("encodeBase56 / vector # " + vectorNumber +
+                    ": Encoding should match result in vector file", expectedResult, encoded);
+
+            assertArrayEquals("decodeBase56 / vector # " + vectorNumber +
+                    ": Decoded result should match input in vector file", input, decoded);
+
+            vectorNumber++;
+        }
+    }
+
+    @Test
+    public void testEncodeAndDecodeVectors() throws Exception {
+        List<List<String>> vectors = TestHelper.parseVectorCsvFile(
+                "base56-vectors.txt", true, true );
+
+        int vectorNumber = 1;
+        for (List<String> vector: vectors) {
+            byte[] input = EncryptionUtils.hex2Byte(vector.get(0));
+            String lineNum = vector.get(1);
+            String expectedResult = vector.get(2);
+            String expectedCheckChar = vector.get(3);
+
+            String encoded = EncryptionUtils.encodeBase56(input);
+
+            // Remove check character, this will be verified separately
+            encoded = encoded.substring(0, encoded.length()-1);
+
+            assertEquals("encodeBase56 / vector # " + vectorNumber +
+                    ": Encoding should match result in vector file", expectedResult, encoded);
+
+            //TODO: Add check character validation
+            // This cannot be done sanely at the moment, because the check character
+            // creation is inlined in the encode/decode/verify functions, and there is
+            // no way to pass in the line number. This should definitely be pulled out
+            // into a separate function!
+
+            //TODO: Add decoding validation
+            // This cannot be done sanely at the moment, because the check character
+            // creation is inlined in the encode/decode/verify functions, and there is
+            // no way to pass in the line number. This should definitely be pulled out
+            // into a separate function!
+
+            vectorNumber++;
+        }
     }
 }
