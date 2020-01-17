956d08f4447b47992095b7c5f06a7ac4d944dcdd
Alex Hauser <alex@hausers.cc>
2020-01-17 12:43:11 +0100

    Add TextValidator helper class

---
+++ app/src/main/java/org/ea/sqrl/utils/TextValidator.java
@@ -0,0 +1,31 @@
+package org.ea.sqrl.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+/**
+ * Abstract helper class for easily implementing on-the-fly validation of
+ * EditText input fields.
+ */
+public abstract class TextValidator implements TextWatcher {
+    private final TextView textView;
+
+    public TextValidator(TextView textView) {
+        this.textView = textView;
+    }
+
+    public abstract void validate(TextView textView, String text);
+
+    @Override
+    final public void afterTextChanged(Editable s) {
+        String text = textView.getText().toString();
+        validate(textView, text);
+    }
+
+    @Override
+    final public void beforeTextChanged(CharSequence s, int start, int count, int after) { /* Don't care */ }
+
+    @Override
+    final public void onTextChanged(CharSequence s, int start, int before, int count) { /* Don't care */ }
+}
